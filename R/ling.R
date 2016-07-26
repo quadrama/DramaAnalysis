2d858d1ca39c67fbbf218b63c038b2b48ce523bd
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-07-26 17:16:59 +0200

    added pos tag collections

---
+++ R/ling.R
@@ -0,0 +1,11 @@
+#' Provides lists of groups of pos tags for various word classes.
+#'
+#' @export
+postags <- list(
+  de=list(
+    v=c("VAFIN","VAIMP","VAINF","VAPP","VMFIN","VMINF","VVFIN","VVIMP","VVINF","VVIZU","VVPP"),
+    n=c("NN"),
+    vv=c("VVFIN","VVIMP","VVINF","VVIZU","VVPP"),
+    adj=c("ADJA", "ADJD")
+  )
+)
