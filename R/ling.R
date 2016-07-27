b418fa559a40fcf5cbd1a438b918d77adf40d67e
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-07-27 14:30:50 +0200

    add word pos tags

--- R/ling.R
+++ R/ling.R
@@ -6,6 +6,12 @@ postags <- list(
     v=c("VAFIN","VAIMP","VAINF","VAPP","VMFIN","VMINF","VVFIN","VVIMP","VVINF","VVIZU","VVPP"),
     n=c("NN"),
     vv=c("VVFIN","VVIMP","VVINF","VVIZU","VVPP"),
-    adj=c("ADJA", "ADJD")
+    adj=c("ADJA", "ADJD"),
+    words=c("ADJA", "ADJD", "ADV", "APPR", "APPRART", "APZR", "ART", "CARD", "FM", "ITJ", "KOKOM",
+            "KON","KOUI","KOUS","NE","NN","PDAT","PDS","PIAT","PIDAT","PIS","PPER","PPOSAT", "PPOSS", "PRELAT",
+            "PRELS","PRF","PROAV","PTKA","PTKANT","PTKNEG","PTKVZ",
+            "PTKZU","PWAT","PWAV","PWS","TRUNC","VAFIN","VAIMP",
+            "VAINF","VAPP","VMFIN","VMINF","VVFIN","VVIMP","VVINF",
+            "VVIZU","VVPP","XY","APPO","VMPP")
   )
 )
