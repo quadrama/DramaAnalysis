41740493d283dc5e61d0a5f2b4636dce4139a786
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-08-02 19:34:03 +0200

    switched to real url on zwergdrossel

--- R/init.R
+++ R/init.R
@@ -1,5 +1,5 @@
 environment <- new.env()
 
 # This needs to change at some point
-environment$url <- "http://localhost:8080/drama.web/"
+environment$url <- "http://zwergdrossel.ims.uni-stuttgart.de:8080/drama.web/"
 
