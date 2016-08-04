31a2cca49baefc241aef26a92f82ab6ae25f49ab
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-08-04 04:06:23 +0200

    Fixed normalisation issues, values now the same as on web page

--- R/text.R
+++ R/text.R
@@ -145,7 +145,7 @@ limit_figures <- function(t, minTokens=100) {
 }
 
 #' @export
-count_word_fields <- function(t, fieldnames=c(), normalize = FALSE, names=FALSE) {
+count_word_fields <- function(t, fieldnames=c(), normalize = FALSE, names=FALSE, boost = 100) {
   baseurl <- "https://raw.githubusercontent.com/quadrama/metadata/master/fields/"
   bylist <- list(t$drama, t$Speaker.figure_id)
   if (names == TRUE)
@@ -154,31 +154,32 @@ count_word_fields <- function(t, fieldnames=c(), normalize = FALSE, names=FALSE)
   for (field in fieldnames) {
     url <- paste(baseurl, field, ".txt", sep="")
     list <- read.csv(url, header=F, fileEncoding = "UTF-8")
-    r <- cbind(r,  count_word_field(t, tolower(list$V1), normalize = FALSE, names=names)[,3])
+    r <- cbind(r,  count_word_field(t, tolower(list$V1), normalize.by.figure =FALSE, normalize.by.field = TRUE,names=names)[,3])
   }
   colnames(r) <- c("drama", "figure", fieldnames)
   if (normalize == TRUE) {
     tokens <- aggregate(t$Token.surface, by=bylist, function(x) { length(x) })
     r[,-(1:2)] <- r[,-(1:2)] / ave(tokens[[3]], tokens[1:2], FUN=function(x) {x})
   }
-  if (names == TRUE)
-    #r$figure <- fnames
+  r[,-(1:2)] <- r[,-(1:2)] * boost
   r
 }
 
-count_word_field <- function(t, wordfield=c(), names = FALSE, normalize = FALSE) {
+count_word_field <- function(t, wordfield=c(), names = FALSE, normalize.by.figure = FALSE, normalize.by.field = FALSE) {
   bylist <- list(t$drama, t$Speaker.figure_id)
   if (names == TRUE)
     bylist <- list(t$drama, t$Speaker.figure_surface)
 
   r <- aggregate(t$Token.surface, by=bylist, function(x) {
-    #print(x[tolower(x) %in% wordfield])
-    length(x[tolower(x) %in% wordfield])
+    if (normalize.by.field == TRUE)
+      length(x[tolower(x) %in% wordfield])/length(wordfield)
+    else
+      length(x[tolower(x) %in% wordfield])
   })
 
 
   colnames(r) <- c("drama", "figure", "x")
-  if (normalize == TRUE) {
+  if (normalize.by.figure == TRUE) {
     for (i in 1:nrow(r)) {
       r[i,]$x <- r[i,]$x / length(t[t$drama == r[i,1] & t$Speaker.figure_id == r[i,2],]$Token.lemma)
     }
