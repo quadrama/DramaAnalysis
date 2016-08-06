c4c5c0fb6cc8f976636b6348e31bb78db8f9f1b8
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-08-06 09:15:21 +0200

    switch load.R and data.R

---
+++ R/load.R
@@ -0,0 +1,29 @@
+id2url <- function(id) {
+  paste(environment$url, "annotations/", id, sep="")
+}
+
+#' Loads a CSV-formatted text from the server,
+#' assuming the main server url has been set correctly.
+#'
+#'
+#' @param ids A vector containing drama ids to be downloaded
+#' @param tokens If set to true, the table also contains each token in an utterance
+#' @export
+load_text <- function(ids, tokens=FALSE) {
+  r <- data.frame(c())
+  s <- ""
+  if (tokens == TRUE) {
+    s <- "/de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"
+  }
+  for (a in ids) {
+    myurl <- paste(id2url(a), "/de.unistuttgart.ims.drama.api.Utterance", s, sep="")
+    print(myurl)
+    data <- load_from_url(myurl)
+    r <- rbind(r,data)
+  }
+  r
+}
+
+load_from_url <- function(url) {
+  read.csv(url, header=TRUE, fileEncoding="UTF-8")
+}
