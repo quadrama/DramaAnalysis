d80ccd47413eb6e242755ce929b0334cc270d936
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-09-16 13:47:45 +0200

    We can now use the function make_stylo_table to also generate tables by text

--- R/text.R
+++ R/text.R
@@ -130,6 +130,7 @@ generate_word_cloud <- function(freq_table, min.freq=10,column=1, colors="black"
 #' @param t The text table, potentially covering multiple texts
 #' @param accepted.pos A list of accepted pos tags
 #' @param names Whether to use figure names or ids
+#' @param by.figure Wether the count is by figure or by text
 #' @param column The column name we should use (should be either Token.surface or Token.lemma)
 #' @export
 #' @examples
@@ -139,20 +140,22 @@ generate_word_cloud <- function(freq_table, min.freq=10,column=1, colors="black"
 #' @examples
 #' t <- load_text(read.csv("http://localhost:8080/drama.web/dramas", header=FALSE)[,], tokens=T)
 #' tl <- limit_figures(t, minTokens=1000)
-#' stylo_table <- make_stylo_table(tl, names=TRUE)
+#' stylo_table <- make_stylo_table(tl, names=TRUE, by.figure=TRUE)
 #' stylo(gui=F, frequencies = stylo_table, network=T, write.png.file=T, analysis.type="BCT")
-make_stylo_table <- function(t, accepted.pos=postags$de$words, names=FALSE, column="Token.surface") {
+make_stylo_table <- function(t, accepted.pos=postags$de$words, names=FALSE, column="Token.surface", by.figure=FALSE) {
   ft <- t
   if (length(accepted.pos) > 0)
     ft <- t[t$Token.pos %in% accepted.pos,]
-  if (names == TRUE)
-    r <- do.call(rbind, tapply(ft[[column]], paste(ft$drama, ft$Speaker.figure_surface), function(x){prop.table(table(x))}))
+  if (by.figure == FALSE)
+    index <- paste(ft$drama)
+  else if (names == TRUE)
+    index <- paste(ft$drama, ft$Speaker.figure_surface)
   else
-    r <- do.call(rbind, tapply(ft[[column]], paste(ft$drama, ft$Speaker.figure_id), function(x){prop.table(table(x))}))
+    index <- paste(ft$drama, ft$Speaker.figure_id)
+  r <- do.call(rbind, tapply(ft[[column]], index, function(x){prop.table(table(x))}))
   r[,order(colSums(r),decreasing=TRUE)]
+}
 
-
-  }
 
 #' @export
 limit.figures.by.rank <- function(t, maxRank=10) {
