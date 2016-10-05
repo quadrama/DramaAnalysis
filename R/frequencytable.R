2c998f5f0291904e5253e63411558afdb68dc1e8
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-10-05 15:21:29 +0200

    renamed function from frequency.table to frequencytable

--- R/frequencytable.R
+++ R/frequencytable.R
@@ -4,16 +4,18 @@
 #' @param names Whether to use figure names or ids
 #' @param by.figure Wether the count is by figure or by text
 #' @param column The column name we should use (should be either Token.surface or Token.lemma)
-#' @export
 #' @examples
 #' data(rksp.0)
-#' st <- frequency.table(rksp.0)
+#' st <- frequencytable(rksp.0)
 #' @examples
+#' \dontrun{
 #' t <- load.text(read.csv("http://localhost:8080/drama.web/dramas", header=FALSE)[,], tokens=T)
 #' tl <- limit.figures.by.tokens(t, minTokens=1000)
-#' stylo_table <- frequency.table(tl, names=TRUE, by.figure=TRUE)
+#' stylo_table <- frequencytable(tl, names=TRUE, by.figure=TRUE)
 #' stylo(gui=F, frequencies = stylo_table, network=T, write.png.file=T, analysis.type="BCT")
-frequency.table <- function(t, accepted.pos = postags$de$words, names=FALSE, column="Token.surface", by.figure=FALSE) {
+#' }
+#' @export
+frequencytable <- function(t, accepted.pos = postags$de$words, names=FALSE, column="Token.surface", by.figure=FALSE) {
   ft <- t
   if (length(accepted.pos) > 0)
     ft <- t[t$Token.pos %in% accepted.pos,]
