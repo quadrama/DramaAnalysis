4e35de74acb02230f46ddac7b96e882493a72cc6
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-10-05 15:49:57 +0200

    test files need to start with "test" ...

--- tests/testthat/1.R
+++
@@ -1,3 +0,0 @@
-data(rksp.0)
-
-expect_that(length(rksp.0), equals(25365))
---
+++ tests/testthat/test-data.R
@@ -0,0 +1,7 @@
+data(rksp.0)
+data(vndf.0)
+
+expect_that(length(rksp.0), equals(9))
+expect_that(length(vndf.0), equals(9))
+expect_that(length(rksp.0$begin), equals(25365))
+expect_that(length(vndf.0$begin), equals(28542))
