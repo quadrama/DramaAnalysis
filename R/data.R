0eee56d0a129feb5c06fbf0d88c3f0b691aa639d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-04-25 08:30:15 +0200

    updated documentation for `rksp.0.text`

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' The data set exported from Emilia Galotti
 #' @format A data frame with 25365 rows and 9 columns:
 #' \describe{
-#'  \item{drama}{The drama id}
 #'  \item{length}{The length of the drama (in tokens)}
+#'  \item{drama}{The drama id}
 #'  \item{begin}{The beginning of an utterance (character position)}
 #'  \item{end}{The end of an utterance (character position)}
 #'  \item{Speaker.figure_surface}{The surface description of a figure}
