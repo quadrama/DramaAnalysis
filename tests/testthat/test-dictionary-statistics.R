84cbb5d252abc1787c47dc4c5a4f8db7e835eaba
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-04-25 04:43:29 +0200

    more tests

--- tests/testthat/test-dictionary-statistics.R
+++ tests/testthat/test-dictionary-statistics.R
@@ -4,3 +4,11 @@ dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön"), name
 
 expect_that(length(dstat), equals(3))
 expect_that(dstat[7,3], equals(1))
+
+dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön"), names=TRUE, normalize.by.figure = TRUE)
+expect_that(length(dstat), equals(3))
+expect_that(dstat[7,3], equals(0.001308901))
+
+dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön","gut"), names=TRUE, normalize.by.figure = FALSE, normalize.by.field = TRUE)
+expect_that(length(dstat), equals(3))
+expect_that(dstat[7,3], equals(0.5))
