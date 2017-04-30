f16af6787d029d7404cc9b5aa13b810369ce69d8
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-04-30 07:28:50 +0200

    fix #14

--- tests/testthat/test-dictionary-statistics.R
+++ tests/testthat/test-dictionary-statistics.R
@@ -7,7 +7,7 @@ expect_that(dstat[7,3], equals(1))
 
 dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön"), names=TRUE, normalize.by.figure = TRUE)
 expect_that(length(dstat), equals(3))
-expect_that(dstat[7,3], equals(0.001308901))
+expect_that(dstat[7,3], equals(0.0007209805))
 
 dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön","gut"), names=TRUE, normalize.by.figure = FALSE, normalize.by.field = TRUE)
 expect_that(length(dstat), equals(3))
--- tests/testthat/test-utterance_statistics.R
+++ tests/testthat/test-utterance_statistics.R
@@ -4,13 +4,13 @@ data(vndf.0.text)
 ustat <- utterance_statistics(rksp.0.text, num.figures = 10, normalize.by.drama.length=FALSE)
 
 expect_that(length(ustat), equals(5))
-expect_that(length(ustat$drama), equals(785))
+expect_that(length(ustat$drama), equals(781))
 expect_that(median(ustat$utterance_length), equals(18))
 expect_that(median(ustat[ustat$figure=="Odoardo",5]), equals(13.5))
 
 ustat <- utterance_statistics(rksp.0.text, num.figures = 10, normalize.by.drama.length=TRUE)
 
 expect_that(length(ustat), equals(5))
-expect_that(length(ustat$drama), equals(785))
+expect_that(length(ustat$drama), equals(781))
 expect_that(median(ustat$utterance_length), equals(0.0007096393))
 expect_that(median(ustat[ustat$figure=="Odoardo",5]), equals(0.0005322295))
