e3c75092703cf9b7f2b7c1d491dffcd833ccb606
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-05-04 10:06:46 +0200

    utterance_statistics() → utteranceStatistics()

--- R/utterance_statistics.R
+++ R/utterance_statistics.R
@@ -1,22 +1,22 @@
 #' This method calculates the length of each utterance, organised by figure and drama.
 #' @param t The dramatic text(s)
-#' @param num.figures The maximal number of figures per drama to include. Default: 10. Set to FALSE to include all figures.
-#' @param normalize.by.drama.length Logical value. If true, the resulting values will be normalized by the length of the drama.
+#' @param numberOfFigures The maximal number of figures per drama to include. Default: 10. Set to FALSE to include all figures.
+#' @param normalizeByDramaLength Logical value. If true, the resulting values will be normalized by the length of the drama.
 #' @export
 #' @examples
 #' data(rksp.0.text)
 #' num_figures <- 5
-#' ustat <- utterance_statistics(rksp.0.text, num.figures = num_figures)
+#' ustat <- utteranceStatistics(rksp.0.text, numberOfFigures = num_figures)
 #' \dontrun{
 #' boxplot(ustat$utterance_length ~ ustat$figure,
 #'    col=qd.colors[1:num_figures],
 #'    las=2, frame=FALSE)
 #' }
 #' @importFrom stats aggregate
-utterance_statistics <- function(t, num.figures=10, normalize.by.drama.length = TRUE) {
+utteranceStatistics <- function(t, numberOfFigures=10, normalizeByDramaLength = TRUE) {
 
-  if (typeof(num.figures) == "double") {
-    t <- limit.figures.by.rank(t, maxRank = num.figures)
+  if (typeof(numberOfFigures) == "double") {
+    t <- limit.figures.by.rank(t, maxRank = numberOfFigures)
   }
   # utterance statistics
   ulength <- aggregate(t$Token.surface, by=list(t$drama, t$Speaker.figure_surface, t$begin, t$length), length)
@@ -24,7 +24,7 @@ utterance_statistics <- function(t, num.figures=10, normalize.by.drama.length = 
   colnames(ulength) <- c("drama", "figure", "begin", "drama_length","utterance_length")
 
   # normalize by drama length
-  if (normalize.by.drama.length == TRUE) {
+  if (normalizeByDramaLength == TRUE) {
     ulength$utterance_length <- ulength$utterance_length / ulength$drama_length
   }
   # skip empty factor levels
