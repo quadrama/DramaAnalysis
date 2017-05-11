ca561c9d25118eba3dbda99984607da1f153f192
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-05-11 22:06:41 +0200

    #13 dictionary.statistics() → dictionaryStatistics(), dictionaryStatisticsSingle() → dictionaryStatisticsSingle()

--- tests/testthat/test-dictionary-statistics.R
+++ tests/testthat/test-dictionary-statistics.R
@@ -1,14 +1,14 @@
 data(rksp.0.text)
 
-dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön"), names=TRUE)
+dstat <- dictionaryStatisticsSingle(rksp.0.text, wordfield = c("schön"), names=TRUE)
 
 expect_that(length(dstat), equals(3))
 expect_that(dstat[6,3], equals(1))
 
-dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön"), names=TRUE, normalize.by.figure = TRUE)
+dstat <- dictionaryStatisticsSingle(rksp.0.text, wordfield = c("schön"), names=TRUE, normalizeByFigure = TRUE)
 expect_that(length(dstat), equals(3))
 expect_that(dstat[6,3], equals(0.0013089005))
 
-dstat <- dictionary.statistics.single(rksp.0.text, wordfield = c("schön","gut"), names=TRUE, normalize.by.figure = FALSE, normalize.by.field = TRUE)
+dstat <- dictionaryStatisticsSingle(rksp.0.text, wordfield = c("schön","gut"), names=TRUE, normalizeByFigure = FALSE, normalizeByField = TRUE)
 expect_that(length(dstat), equals(3))
 expect_that(dstat[6,3], equals(0.5))
