8a316c321494713fc1dad370723948b85cde7fb1
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-05-12 06:18:32 +0200

    #13 parameter only.presence → onlyPresence

--- R/text.R
+++ R/text.R
@@ -58,7 +58,7 @@ tfidf <- function(ftable) {
 #' the three components matrix, drama, and figure
 #' @param mtext The text including Act and Scene markings
 #' @param by A string, either "Act" or "Scene"
-#' @param only.presence If TRUE, the resulting matrix only contains 
+#' @param onlyPresence If TRUE, the resulting matrix only contains 
 #' logical values for stage presence
 #' @seealso DramaAnalysis::load.text2()
 #' @export
@@ -66,14 +66,14 @@ tfidf <- function(ftable) {
 #' data(rksp.0.mtext)
 #' cfg <- configuration(rksp.0.mtext)
 #' 
-configuration <- function(mtext, by="Act", only.presence=FALSE) {
+configuration <- function(mtext, by="Act", onlyPresence=FALSE) {
   
   if (by=="Scene") {
     c <- configuration.scene(mtext)
   } else {
     c <- configuration.act(mtext)
   }
-  if (only.presence)
+  if (onlyPresence)
     c$matrix <- c$matrix>0
   c
 }
