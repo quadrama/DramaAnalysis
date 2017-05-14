cce0865236b70099e0a9e1e24c7a06db6c05b2d7
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-05-14 10:13:53 +0200

    #13

--- R/genre.R
+++ R/genre.R
@@ -2,7 +2,7 @@
 #' Calculates correlation of a frequency table with an outcome list according to given method
 #' @param text.ft A matrix, containing words in columns and figures (or plays) in rows
 #' @param outcomes A list of outcomes, will be cast as numeric
-#' @param minimal.frequency An integer. Words that appear less than x times can be filtered in advance
+#' @param minimalFrequency An integer. Words that appear less than x times can be filtered in advance
 #' @param method The correlation method, passed on to cor()
 #' @export
 #' @importFrom stats cor
@@ -11,8 +11,8 @@
 #' rksp.0.ft <- frequencytable(rksp.0.text, byFigure=TRUE, names=TRUE)
 #' g <- factor(c("m","m","m","m","f","m","m","m","f","m","m","f","m"))
 #' rksp.0.cor <- correlationAnalysis(rksp.0.ft,g)
-correlationAnalysis <- function(text.ft, outcomes,  method="spearman",minimal.frequency=10) {
-  text.ft.filtered <- data.frame(text.ft[,colSums(text.ft!=0) > minimal.frequency])
+correlationAnalysis <- function(text.ft, outcomes,  method="spearman",minimalFrequency=10) {
+  text.ft.filtered <- data.frame(text.ft[,colSums(text.ft!=0) > minimalFrequency])
   outcomes <- as.numeric(outcomes)
   text.cor <- data.frame(cor( text.ft.filtered, y=outcomes, method=method))
   colnames(text.cor) <- c("cor")
