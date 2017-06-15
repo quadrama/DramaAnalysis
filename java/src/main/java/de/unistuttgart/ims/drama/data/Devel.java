d98294d5b1154121ab003de14412c44f4b692e9f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-06-15 17:23:56 +0200

    main method for test and development

---
+++ java/src/main/java/de/unistuttgart/ims/drama/data/Devel.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.ims.drama.data;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.xml.sax.SAXException;
+
+public class Devel {
+
+	public static void main(String[] args) throws ClassNotFoundException, UIMAException, SAXException, IOException {
+		DataLoader dl = new DataLoader("/Users/reiterns/Documents/QuaDramA/Data");
+		System.out.println(dl.getAnnotations(new String[] { "tc:tc0623", "tg:rksp.0" },
+				de.unistuttgart.ims.drama.api.Author.class.getName(), null));
+
+	}
+
+}
