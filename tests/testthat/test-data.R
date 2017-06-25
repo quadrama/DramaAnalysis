6c18a83e0e823b90e015d43e1613fa1932e67a9d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-06-25 10:50:19 +0200

    updated tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,7 +1,7 @@
 data(rksp.0)
 data(vndf.0)
 
-expect_that(length(rksp.0$mtext), equals(15))
-expect_that(length(vndf.0$mtext), equals(15))
+expect_that(length(rksp.0$mtext), equals(16))
+expect_that(length(vndf.0$mtext), equals(16))
 expect_that(length(rksp.0$mtext$begin), equals(25365))
 expect_that(length(vndf.0$mtext$begin), equals(28542))
