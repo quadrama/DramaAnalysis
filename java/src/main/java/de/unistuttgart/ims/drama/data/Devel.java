3778e2aad80f7b0a67a5cf0e0ea6b07d116b993e
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-07-02 09:21:52 +0200

    improved readability

--- java/src/main/java/de/unistuttgart/ims/drama/data/Devel.java
+++ java/src/main/java/de/unistuttgart/ims/drama/data/Devel.java
@@ -5,17 +5,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.uima.UIMAException;
+import org.apache.uima.jcas.cas.TOP;
 import org.xml.sax.SAXException;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import de.unistuttgart.ims.drama.api.Date;
+import de.unistuttgart.ims.drama.api.Author;
+import de.unistuttgart.ims.drama.api.Translator;
 import de.unistuttgart.ims.drama.api.Utterance;
 
 public class Devel {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws ClassNotFoundException, UIMAException, SAXException, IOException {
 		DataLoader dl = new DataLoader("/Users/reiterns/Documents/QuaDramA/Data");
-		System.out.println(dl.getAnnotations(new String[] { "tc:tc0623", "tg:rksp.0" }, Date.class, Date.class));
+
+		Class<? extends TOP>[] classes = new Class[] { Author.class, Translator.class };
+		String[] ids = new String[] { "tc:tc0623", "tg:rksp.0", "tg:vndf.0" };
+
+		System.out.println(dl.getAnnotations(ids, classes));
 		System.exit(1);
 		FileWriter fw = new FileWriter(new File("target/test.csv"));
 		fw.write(dl.getAnnotations(new String[] { "tg:rksp.0", "tc:tc0623" }, Utterance.class, Token.class));
