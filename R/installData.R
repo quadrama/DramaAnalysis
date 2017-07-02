e332065b1403e42b4592267af0f6daa8650bb07b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-07-02 09:27:55 +0200

    comments in code

--- R/installData.R
+++ R/installData.R
@@ -6,12 +6,35 @@
 #' @param baseUrl The github user (or group)
 #' @importFrom utils download.file unzip
 #' @export
-installCollectionData <- function(dataDirectory=getOption("qd.datadir"),branchOrCommit="master",repository="metadata",baseUrl="https://github.com/quadrama/") {
+installCollectionData <- function(dataDirectory=getOption("qd.datadir"),
+                                  branchOrCommit="master",
+                                  repository="metadata",
+                                  baseUrl="https://github.com/quadrama/") {
+  # create temporary file to download
   tf <- tempfile()
-  utils::download.file(paste0(baseUrl,repository,"/archive/",branchOrCommit,".zip"), destfile=tf)
+  
+  # download repo as zip file
+  utils::download.file(paste0(baseUrl,
+                              repository,
+                              "/archive/",
+                              branchOrCommit,".zip"), 
+                       destfile=tf)
+  
+  # unzip for temp file
   utils::unzip(tf,exdir=dataDirectory)
-  repoDirectory = file.path(dataDirectory,paste(repository,branchOrCommit,sep="-"))
+  
+  repoDirectory = file.path(dataDirectory, 
+                            paste(repository, branchOrCommit,sep="-"))
+  # remove old collections-directory
   unlink(file.path(dataDirectory,"collections"))
-  file.rename(from=file.path(repoDirectory,"collections"),to=file.path(dataDirectory,"collections"))
+  
+  # move new dir into place
+  file.rename(from=file.path(repoDirectory,"collections"),
+              to=file.path(dataDirectory,"collections"))
+  
+  # remove repo directory
   unlink(repoDirectory,recursive = TRUE)
+  
+  # remove temp file
+  unlink(tf)
 }
