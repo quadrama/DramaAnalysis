87562f4ec798c7dd3eea11464e642a5cc091a184
Tim Strohmayer <st151528@stud.uni-stuttgart.de>
2017-10-20 14:15:53 +0200

    Add byScene and byAct parameters for frequencytable-function.

--- R/frequencytable.R
+++ R/frequencytable.R
@@ -3,6 +3,8 @@
 #' @param acceptedPOS A list of accepted pos tags
 #' @param names Whether to use figure names or ids
 #' @param byFigure Wether the count is by figure or by text
+#' @param byScene Wether the count is by scene or by drama
+#' @param byAct Whether the count is by act or by drama
 #' @param column The column name we should use (should be either Token.surface or Token.lemma)
 #' @param sep The separation character that goes between drama name and figure (if applicable)
 #' @param normalize Whether to normalize values or not
@@ -20,21 +22,51 @@
 #' stylo(gui=F, frequencies = stylo_table)
 #' }
 #' @export
-frequencytable <- function(t, acceptedPOS = postags$de$words, names=FALSE, column="Token.surface", byFigure=FALSE, sep="|", normalize=TRUE, sortResult=FALSE) {
+frequencytable <- function(t, acceptedPOS = postags$de$words, names=FALSE, column="Token.surface", byFigure=FALSE, sep="|", normalize=TRUE, sortResult=FALSE, byAct=FALSE, byScene=FALSE) {
   ft <- t
   if (length(acceptedPOS) > 0)
     ft <- t[t$Token.pos %in% acceptedPOS,]
   
   
   if (byFigure == FALSE) {
-    xt <- stats::xtabs(~drama + ft[,get(column)], data=ft)
-    r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    
+    if (byScene == TRUE) {
+      xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    } else if (byAct == TRUE) {
+      xt <- stats::xtabs(~ paste(drama,Number.Act,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    } else {
+      xt <- stats::xtabs(~drama + ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    }
+    
   } else if (names == TRUE) {
-    xt <- stats::xtabs(~ paste(drama,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft)
-    r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    
+    if (byScene == TRUE) {
+      xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    } else if (byAct == TRUE) {
+      xt <- stats::xtabs(~ paste(drama,Number.Act,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    } else {
+      xt <- stats::xtabs(~ paste(drama,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    }
+    
   } else {
-    xt <- stats::xtabs(~paste(drama,Speaker.figure_id,sep=sep)+ft[,get(column)], data=ft)
-    r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    
+    if (byScene == TRUE) {
+      xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    } else if (byAct == TRUE) {
+      xt <- stats::xtabs(~ paste(drama,Number.Act,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    } else {
+      xt <- stats::xtabs(~ paste(drama,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft)
+      r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
+    }
+    
   }
   
   
@@ -47,26 +79,4 @@ frequencytable <- function(t, acceptedPOS = postags$de$words, names=FALSE, colum
   }
   
   r
-}
-
-
-#' Extract bigrams instead of words (currently not taking utterance boundaries into account)
-#' @param t The text
-#' @param acceptedPOS A list of accepted pos tags
-#' @param names Whether to use figure names or ids
-#' @param byFigure Wether the count is by figure or by text
-#' @param cols The column names we should use (should be either Token.surface or Token.lemma)
-#' @keywords internal
-frequencytable2 <- function(t, acceptedPOS = postags$de$words, names=FALSE, cols=c("Token.surface", "Token.surface"), byFigure=FALSE) {
-  ft <- t
-  if (length(acceptedPOS) > 0)
-    ft <- t[t$Token.pos %in% acceptedPOS,]
-  if (byFigure == FALSE)
-    index <- paste(ft$drama)
-  else if (names == TRUE)
-    index <- paste(ft$drama, ft$Speaker.figure_surface)
-  else
-    index <- paste(ft$drama, ft$Speaker.figure_id)
-  r <- do.call(rbind, tapply(paste(ft[[cols[1]]], ft[[cols[2]]][-1]), index, function(x){prop.table(table(x))}))
-  r[,order(colSums(r),decreasing=TRUE)]
 }
