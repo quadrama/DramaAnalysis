47e3ba136159c91744acd690d2ef49493e251227
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-10-25 17:20:20 +0200

    UtterancesWithTokens now contains two Mentioned columns

--- java/src/main/java/de/unistuttgart/ims/drama/data/CSVVariant.java
+++ java/src/main/java/de/unistuttgart/ims/drama/data/CSVVariant.java
@@ -13,6 +13,7 @@ import org.apache.uima.jcas.JCas;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.unistuttgart.ims.drama.api.Act;
 import de.unistuttgart.ims.drama.api.Drama;
+import de.unistuttgart.ims.drama.api.FigureMention;
 import de.unistuttgart.ims.drama.api.Scene;
 import de.unistuttgart.ims.drama.api.Speaker;
 import de.unistuttgart.ims.drama.api.Utterance;
@@ -29,7 +30,8 @@ public enum CSVVariant {
 			break;
 		default:
 			p.printRecord("corpus", "drama", "begin", "end", "Speaker.figure_surface", "Speaker.figure_id",
-					"Token.surface", "Token.pos", "Token.lemma", "length");
+					"Token.surface", "Token.pos", "Token.lemma", "length", "Mentioned.figure_surface",
+					"Mentioned.figure_id");
 		}
 	}
 
@@ -73,20 +75,39 @@ public enum CSVVariant {
 	}
 
 	private void convertUtterancesWithTokens(JCas jcas, CSVPrinter p) throws IOException {
+		Map<Token, Collection<FigureMention>> mentionMap = JCasUtil.indexCovering(jcas, Token.class,
+				FigureMention.class);
 		Drama drama = JCasUtil.selectSingle(jcas, Drama.class);
 		int length = JCasUtil.select(jcas, Token.class).size();
+		Set<FigureMention> used = new HashSet<FigureMention>();
 		for (Utterance utterance : JCasUtil.select(jcas, Utterance.class)) {
 			for (Speaker speaker : DramaUtil.getSpeakers(utterance)) {
 				for (int i = 0; i < speaker.getCastFigure().size(); i++) {
 					for (Token token : JCasUtil.selectCovered(Token.class, utterance)) {
 						p.print(drama.getCollectionId());
 						p.print(drama.getDocumentId());
+						p.print(utterance.getBegin());
+						p.print(utterance.getEnd());
 						p.print(speaker.getCastFigure(i).getNames(0));
 						p.print(speaker.getCastFigure(i).getXmlId(0));
 						p.print(token.getCoveredText());
 						p.print(token.getPos().getPosValue());
 						p.print(token.getLemma().getValue());
 						p.print(length);
+						if (mentionMap.containsKey(token)) {
+							FigureMention fm = selectLongest(mentionMap.get(token));
+							if (used.contains(fm)) {
+								p.print(null);
+								p.print(null);
+							} else {
+								p.print(fm.getCastFigure().getNames(0));
+								p.print(fm.getCastFigure().getXmlId(0));
+								used.add(fm);
+							}
+						} else {
+							p.print(null);
+							p.print(null);
+						}
 						p.println();
 					}
 				}
@@ -94,4 +115,19 @@ public enum CSVVariant {
 		}
 	}
 
+	private FigureMention selectLongest(Collection<FigureMention> coll) {
+		int l = -1;
+		FigureMention fm = null;
+		for (FigureMention ment : coll) {
+			int cl = ment.getEnd() - ment.getBegin();
+			if (cl > l) {
+				l = cl;
+				fm = ment;
+			}
+		}
+		if (fm == null)
+			return coll.iterator().next();
+		else
+			return fm;
+	}
 }
