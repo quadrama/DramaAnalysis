51a542b2732693f680f97489139482da18b0c18c
Tim Strohmayer <st151528@stud.uni-stuttgart.de>
2017-10-25 15:33:17 +0200

    Change logical arguments 'byAct' and 'byScene' into single string argument 'by'. Change default value of 'normalize'-argument to FALSE.

--- R/frequencytable.R
+++ R/frequencytable.R
@@ -3,8 +3,7 @@
 #' @param acceptedPOS A list of accepted pos tags
 #' @param names Whether to use figure names or ids
 #' @param byFigure Wether the count is by figure or by text
-#' @param byScene Wether the count is by scene or by drama
-#' @param byAct Whether the count is by act or by drama
+#' @param by Whether the count is by drama (default), act or scene
 #' @param column The column name we should use (should be either Token.surface or Token.lemma)
 #' @param sep The separation character that goes between drama name and figure (if applicable)
 #' @param normalize Whether to normalize values or not
@@ -22,43 +21,29 @@
 #' stylo(gui=F, frequencies = stylo_table)
 #' }
 #' @export
-frequencytable <- function(t, acceptedPOS = postags$de$words, names=FALSE, column="Token.surface", byFigure=FALSE, sep="|", normalize=TRUE, sortResult=FALSE, byAct=FALSE, byScene=FALSE) {
+frequencytable <- function(t, acceptedPOS = postags$de$words, names=FALSE, column="Token.surface", byFigure=FALSE, sep="|", normalize=FALSE, sortResult=FALSE, by="drama") {
   ft <- t
   if (length(acceptedPOS) > 0)
     ft <- t[t$Token.pos %in% acceptedPOS,]
   
-  
-  if (byFigure == FALSE) {
-    
-    if (byScene == TRUE) {
-      xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,sep=sep) + ~ft[,get(column)], data=ft)
-    } else if (byAct == TRUE) {
-      xt <- stats::xtabs(~ paste(drama,Number.Act,sep=sep) + ~ft[,get(column)], data=ft)
-    } else {
-      xt <- stats::xtabs(~drama + ft[,get(column)], data=ft)
-    }
-    
-  } else if (names == TRUE) {
-    
-    if (byScene == TRUE) {
-      xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft)
-    } else if (byAct == TRUE) {
-      xt <- stats::xtabs(~ paste(drama,Number.Act,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft)
-    } else {
-      xt <- stats::xtabs(~ paste(drama,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft)
-    }
-    
-  } else {
-    
-    if (byScene == TRUE) {
-      xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft)
-    } else if (byAct == TRUE) {
-      xt <- stats::xtabs(~ paste(drama,Number.Act,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft)
-    } else {
-      xt <- stats::xtabs(~ paste(drama,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft)
-    }
-    
-  }
+  switch(by,
+         drama = { 
+           if (byFigure == FALSE) { xt <- stats::xtabs(~drama + ft[,get(column)], data=ft) }
+           else if (names == TRUE) { xt <- stats::xtabs(~ paste(drama,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft) }
+           else { xt <- stats::xtabs(~ paste(drama,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft) }
+         },
+         act = {
+           if (byFigure == FALSE) { xt <- stats::xtabs(~ paste(drama,Number.Act,sep=sep) + ~ft[,get(column)], data=ft) }
+           else if (names == TRUE) { xt <- stats::xtabs(~ paste(drama,Number.Act,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft) }
+           else { xt <- stats::xtabs(~ paste(drama,Number.Act,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft) }
+         },
+         scene = {
+           if (byFigure == FALSE) { xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,sep=sep) + ~ft[,get(column)], data=ft) }
+           else if (names == TRUE) { xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,Speaker.figure_surface,sep=sep) + ~ft[,get(column)], data=ft) }
+           else { xt <- stats::xtabs(~ paste(drama,Number.Act,Number.Scene,Speaker.figure_id,sep=sep) + ~ft[,get(column)], data=ft) }
+         },
+         stop("Please enter valid string-value for argument 'by' (default = 'drama', 'act' or 'scene').")
+  )
   
   r <- as.matrix(stats::ftable(xt, row.vars = c(), col.vars = c()))
   
@@ -79,49 +64,35 @@ frequencytable <- function(t, acceptedPOS = postags$de$words, names=FALSE, colum
 #' @param acceptedPOS A list of accepted pos tags
 #' @param names Whether to use figure names or ids
 #' @param byFigure Wether the count is by figure or by text
-#' @param byScene Wether the count is by scene or by drama
-#' @param byAct Whether the count is by act or by drama
+#' @param by Whether the count is by drama (default), act or scene
 #' @param cols The column names we should use (should be either Token.surface or Token.lemma)
 #' @keywords internal
-frequencytable2 <- function(t, acceptedPOS = postags$de$words, names=FALSE, cols=c("Token.surface", "Token.surface"), byFigure=FALSE, byAct=FALSE, byScene=FALSE) {
+frequencytable2 <- function(t, acceptedPOS = postags$de$words, names=FALSE, cols=c("Token.surface", "Token.surface"), byFigure=FALSE, by="drama") {
   ft <- t
   if (length(acceptedPOS) > 0) {
     ft <- t[t$Token.pos %in% acceptedPOS,]
   }
   
-  if (byFigure == FALSE) {
-    
-    if (byScene == TRUE) {
-      index <- paste(ft$drama, ft$Number.Act, ft$Number.Scene)  
-    } else if (byAct == TRUE) {
-      index <- paste(ft$drama, ft$Number.Act)  
-    } else {
-      index <- paste(ft$drama)
-    }
-    
-  } else if (names == TRUE) {
-    
-    if (byScene == TRUE) {
-      index <- paste(ft$drama, ft$Number.Act, ft$Number.Scene, ft$Speaker.figure_surface)  
-    } else if (byAct == TRUE) {
-      index <- paste(ft$drama, ft$Number.Act, ft$Speaker.figure_surface)  
-    } else {
-      index <- paste(ft$drama, ft$Speaker.figure_surface)
-    }
-  
-  } else {
-    
-    if (byScene == TRUE) {
-      index <- paste(ft$drama, ft$Number.Act, ft$Number.Scene, ft$Speaker.figure_id)  
-    } else if (byAct == TRUE) {
-      index <- paste(ft$drama, ft$Number.Act, ft$Speaker.figure_id)  
-    } else {
-      index <- paste(ft$drama, ft$Speaker.figure_id)
-    }
-    
-  }
+  switch(by,
+         drama = { 
+           if (byFigure == FALSE) { index <- paste(ft$drama) }
+           else if (names == TRUE) { index <- paste(ft$drama, ft$Speaker.figure_surface) }
+           else { index <- paste(ft$drama, ft$Speaker.figure_id) }
+         },
+         act = {
+           if (byFigure == FALSE) { index <- paste(ft$drama, ft$Number.Act) }
+           else if (names == TRUE) { index <- paste(ft$drama, ft$Number.Act, ft$Speaker.figure_surface) }
+           else { index <- paste(ft$drama, ft$Number.Act, ft$Speaker.figure_id) }
+         },
+         scene = {
+           if (byFigure == FALSE) { index <- paste(ft$drama, ft$Number.Act, ft$Number.Scene) }
+           else if (names == TRUE) { index <- paste(ft$drama, ft$Number.Act, ft$Number.Scene, ft$Speaker.figure_surface) }
+           else { index <- paste(ft$drama, ft$Number.Act, ft$Number.Scene, ft$Speaker.figure_id) }
+         },
+         stop("Please enter valid string-value for argument 'by' (default = 'drama', 'act' or 'scene').")
+  )
   
   r <- do.call(rbind, tapply(paste(ft[[cols[1]]], ft[[cols[2]]][-1]), index, function(x){prop.table(table(x))}))
   r[,order(colSums(r),decreasing=TRUE)]
-
+  
 }
