055e94e007d2bf285aee1249360bcf0c7edd22b9
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-11-04 09:54:10 +0100

    more test cases

--- tests/testthat/test-configuration.R
+++ tests/testthat/test-configuration.R
@@ -11,6 +11,12 @@ expect_true("data.table" %in% class(cfg$drama))
 expect_equal(ncol(cfg$matrix),5)
 expect_equal(nrow(cfg$matrix),13)
 
+expect_true(cfg$matrix[1,1])
+expect_false(cfg$matrix[1,2])
+expect_true(cfg$matrix[1,3])
+expect_true(cfg$matrix[1,4])
+expect_true(cfg$matrix[1,5])
+
 cfg <- configuration(rksp.0$mtext, onlyPresence = TRUE, by="Scene")
 expect_length(cfg, 3)
 
