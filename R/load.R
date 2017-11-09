5b2e525439e223d19e4e20fcebe39e6720790351
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-11-09 20:15:39 +0100

    new default data directory

--- R/load.R
+++ R/load.R
@@ -5,7 +5,7 @@
 #' @param collectionDirectory A path to the directory in which collections are stored. 
 #' By default, the directory is called "collection" below the data directory.
 #' @export
-setup <- function(dataDirectory = file.path(path.expand("~"),"QuaDramA","Data"), 
+setup <- function(dataDirectory = file.path(path.expand("~"),"QuaDramA","Data2"), 
                   collectionDirectory = file.path(dataDirectory,"collections")) {
   options(qd.datadir=dataDirectory)
   options(qd.collectionDirectory=collectionDirectory)
