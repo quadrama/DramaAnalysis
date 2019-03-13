408afbd6398a12472832c70628d7294de174a000
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-03-13 07:23:47 +0100

    Updated documentation on rjmw.0 and rksp.0

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 #' @title Data sets
-#' @description \code{rksp.0} represents the data set exported from Emilia Galotti
+#' @description \code{rksp.0} represents the data set exported from Lessigns Emilia Galotti, \code{rjmw.0} is the one exported from Miss Sara Sampson (also written by Lessing).
 #' @format A list containing data.frames and data.table.
 #' @name data
 "rksp.0"
