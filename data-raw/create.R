d3a1ff3dcac1b5cc6ba23ff195eca35ec9698c79
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-03-15 14:03:44 +0100

    updated demo data

--- data-raw/create.R
+++ data-raw/create.R
@@ -4,10 +4,9 @@ setup()
 
 installData("test")
 
-rksp.0 <- list(mtext=loadSegmentedText("test:rksp.0"),
-               char=loadCharacters("test:rksp.0"))
+
+rksp.0 <- loadDrama("test:rksp.0")
 usethis::use_data(rksp.0, overwrite=TRUE)
 
-rjmw.0 <- list(mtext=loadSegmentedText("test:rjmw.0"),
-               char=loadCharacters("test.rjmw.0"))
+rjmw.0 <- loadDrama("test:rjmw.0")
 usethis::use_data(rjmw.0, overwrite=TRUE)
