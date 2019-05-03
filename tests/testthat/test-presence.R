f80f3d4d8ff5073d2f132c1efdd1fb2258e46aa9
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-05-03 19:35:55 +0200

    updated tests

--- tests/testthat/test-presence.R
+++ tests/testthat/test-presence.R
@@ -2,30 +2,28 @@ context("presence()")
 
 data(rksp.0)
 
-pres <- presence(rksp.0$mtext)
-test_that("presence(rksp.0$mtext) 
-          has correct dimensions and produces correct output", {
-  row <- pres[figure=="battista",]
+pres <- presence(rksp.0)
+test_that("presence(rksp.0) has correct dimensions and produces correct output", {
+  row <- pres[pres$character=="battista",]
   expect_equal(row$actives,  4)
   expect_equal(row$passives, 2)
-  row <- pres[figure=="emilia",]
+  row <- pres[pres$character=="emilia",]
   expect_equal(row$actives,  7)
   expect_equal(row$passives, 16)
-  row <- pres[figure=="marinelli",]
+  row <- pres[pres$character=="marinelli",]
   expect_equal(row$actives,  19)
   expect_equal(row$passives, 5)
 })
 
-pres <- presence(rksp.0$mtext, passiveOnlyWhenNotActive = FALSE)
-test_that("presence(rksp.0$mtext, passiveOnlyWhenNotActive = FALSE) 
-          has correct dimensions and produces correct output", {
-  row <- pres[figure=="battista",]
+pres <- presence(rksp.0, passiveOnlyWhenNotActive = FALSE)
+test_that("presence(rksp.0, passiveOnlyWhenNotActive = FALSE) has correct dimensions and produces correct output", {
+  row <- pres[pres$character=="battista",]
   expect_equal(row$actives,  4)
   expect_equal(row$passives, 6)
-  row <- pres[figure=="emilia",]
+  row <- pres[pres$character=="emilia",]
   expect_equal(row$actives,  7)
   expect_equal(row$passives, 23)
-  row <- pres[figure=="marinelli",]
+  row <- pres[pres$character=="marinelli",]
   expect_equal(row$actives,  19)
   expect_equal(row$passives, 24)
 })
