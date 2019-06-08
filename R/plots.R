a3132bc069891c088006bc6514dc1f521230cd9c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-06-08 13:02:07 +0200

    checked plotSpiderWebs() #139

--- R/plots.R
+++ R/plots.R
@@ -34,7 +34,10 @@ plot.QDUtteranceStatistics <- function(x,
 }
 
 #' @title Spider-Webs
-#' @description Generates spider-web like plot
+#' @description Generates spider-web like plot. 
+#' Spider webs may look cool, but they are terrible 
+#' to interpret. You should think of using a bar 
+#' chart to represent the same information. \emph{You have been warned.}
 #' @note radar charts and spider web plots are dangerous, 
 #' they can easily become misleading. They are in this package 
 #' for historic reasons, but should not be used anymore.
@@ -42,8 +45,6 @@ plot.QDUtteranceStatistics <- function(x,
 #' @param mat A data frame that only contains data assignments. Defaults to all columns except for the first three from dstat.
 #' @param names A list of names for the legend
 #' @param symbols Symbols to be used in the plot
-#' @param maxValue The maximal value. Defaults to the highest value in mat.
-#' @param minValue The minimal value. Defaults to the lowest value in mat.
 #' @param cglcol The color for the spider net
 #' @param pcol The line color(s)
 #' @param legend Whether to print a legend
@@ -57,25 +58,36 @@ plot.QDUtteranceStatistics <- function(x,
 #' data(rksp.0)
 #' fnames <- c("Krieg", "Liebe", "Familie", "Ratio","Religion")
 #' ds <- dictionaryStatistics(rksp.0, normalizeByField=TRUE, 
-#'                            fieldnames=fnames, asList=TRUE)
-#' plotSpiderWebs(dstat=ds,max=50)
-plotSpiderWebs <- function(dstat=NULL, mat=dstat$mat, names=dstat$figure, 
+#'                            fieldnames=fnames)
+#' plotSpiderWebs(ds)
+plotSpiderWebs <- function(dstat, 
                            symbols=c(17,16,15,4,8),
-                           maxValue=max(mat),minValue=min(mat), cglcol="black", 
+                           cglcol="black", 
                            legend=TRUE,
                            legend.cex=0.7,
                            legend.pos.x="bottomright",
                            legend.pos.y=NA,
                            legend.horizontal = FALSE,
                            pcol=qd.colors,... ) {
+  stopifnot(inherits(dstat, "QDDictionaryStatistics"))
+  
+  warning("spider webs may look cool, but they are terrible to interpret. You should think of using a bar chart to represent the same information. You have been warned.")
+  
+  mat <- as.matrix(dstat)
+  names = dstat$character
 
-  data <- data.frame(rbind(rep(maxValue,ncol(mat)),rep(minValue,ncol(mat)),mat))
+  maxValue <- max(mat)
+  minValue <- min(mat)
+  
+  data <- data.frame(rbind(rep(maxValue,ncol(mat)),
+                           rep(minValue,ncol(mat)), mat))
   fmsb::radarchart(data, maxmin=TRUE, 
              plwd=1,pcol=pcol,
              pty=symbols,
-             plty=1,vlcex=0.7, ...,
+             plty=1,vlcex=0.7,
              cglcol = cglcol,
              centerzero=TRUE,
+             ...,
              seg=2)
   if (legend) {
     legend(x=legend.pos.x, y=legend.pos.y, horiz=legend.horizontal, col=pcol,
