b70a752fcacacb3daf27c683a15b534b33c7f50b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-06-08 08:47:03 +0200

    Updated documentation on correlationAnalysis() #139

--- R/genre.R
+++ R/genre.R
@@ -1,7 +1,8 @@
 
 #' @title Correlation analysis
 #' @description Calculates correlation of a frequency table with an outcome list according to given method
-#' @param text.ft A matrix, containing words in columns and figures (or plays) in rows
+#' @param text.ft A matrix, containing words in columns and figures (or plays) in rows. 
+#' This can be the result of the \code{\link{frequencytable}} function.
 #' @param outcomes A list of outcomes, will be cast as numeric
 #' @param minimalFrequency An integer. Words that appear less than x times can be filtered in advance
 #' @param method The correlation method, passed on to cor()
@@ -10,9 +11,12 @@
 #' @examples
 #' data(rksp.0)
 #' rksp.0.ft <- frequencytable(rksp.0, byFigure=TRUE)
-#' g <- factor(c("m","m","m","m","f","m","m","m","f","m","m","f","m"))
-#' rksp.0.cor <- correlationAnalysis(rksp.0.ft,g)
-correlationAnalysis <- function(text.ft, outcomes,  method="spearman",minimalFrequency=10) {
+#' g <- c("m","m","m","m","f","m","m","m","f","m","m","f","m")
+#' rksp.0.cor <- correlationAnalysis(rksp.0.ft, g)
+correlationAnalysis <- function(text.ft, 
+                                outcomes,  
+                                method="spearman", 
+                                minimalFrequency = 10) {
   text.ft.filtered <- data.frame(text.ft[,colSums(text.ft!=0) > minimalFrequency])
   outcomes <- as.numeric(outcomes)
   text.cor <- data.frame(cor( text.ft.filtered, y=outcomes, method=method))
