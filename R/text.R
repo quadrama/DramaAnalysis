92ba9aa753513baa5761abb59afe927e9a163952
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-06-19 09:56:43 +0200

    formatting of names no longer in its own .Rd file

--- R/text.R
+++ R/text.R
@@ -38,7 +38,6 @@ qd.colors <- c(rgb(120,28,129, maxColorValue = 255),
 #' @param ... All other arguments are ignored.
 #' @seealso \code{\link[stringr]{str_to_title}}
 #' @importFrom stringr str_to_title
-#' @rdname formatNames
 #' @examples 
 #' data(rksp.0)
 #' ustat <- utteranceStatistics(rksp.0)
