004a4656b6f1bf7ab0b3ad9b9397dd8d5abdd19f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-06-19 17:06:32 +0200

    Moved report() into its own file

--- R/text.R
+++ R/text.R
@@ -247,29 +247,7 @@ extractTopTerms <- function(mat, top=10) {
 
 
 
-#' @title Report
-#' @description generates a report for a specific dramatic text
-#' @param id The id of the text or a list of ids
-#' @param of The output file
-#' @param type The type of the report. "Single" gives a report about a single play, 
-#' while "Compare" can be used to compare multiple editions of a play
-#' @param ... Arguments passed through to the rmarkdown document
-#' @export
-report <- function(id="test:rksp.0", 
-                   of=file.path(getwd(),paste0(unlist(strsplit(id,":",fixed=TRUE))[2], ".html")), 
-                   type=c("Single"),
-                   ...) {
-  force(of)
-  type <- match.arg(type)
-  
-  fileName <- switch(type,
-         Single="Report.Rmd",
-         Compare="Compare-editions.Rmd")
-  rmarkdown::render(system.file(paste0("rmd/",fileName), package="DramaAnalysis"), 
-                    params=list(id=id,col=qd.colors,...), 
-                    output_format = "html_document", 
-                    output_file = of)
-}
+
 
 #' @title Segment
 #' @description This function takes two tables and combines them. The first table is of the 
