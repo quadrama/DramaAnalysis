5687e6e33985d2b5b342e86b9c7bf8c88e73bfc9
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-06-19 09:57:42 +0200

    added variant parameter to loadText(

--- R/load.R
+++ R/load.R
@@ -118,6 +118,7 @@ loadSegments <- function(ids, defaultCollection="qd") {
 #' @param ids A vector containing drama ids to be downloaded
 #' @param includeTokens This argument has no meaning anymore. Tokens are always included.
 #' @param defaultCollection The collection prefix is added if no prefix is found
+#' @param variant The file variant to load
 #' @param unifyCharacterFactors Logical value, defaults to TRUE. Controls whether columns 
 #' representing characters (i.e., Speaker.* and Mentioned.*) are sharing factor levels
 loadText <- function(ids, includeTokens=FALSE, defaultCollection="tg", 
