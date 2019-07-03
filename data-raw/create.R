ddb07e74bf68710cd7886481146998e706c4b26b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-07-03 10:55:34 +0200

    specified version number for data files, so that we keep compatibility with R < 3.5

--- data-raw/create.R
+++ data-raw/create.R
@@ -24,13 +24,13 @@ removeSpecialCharacters <- function(x) {
 
 rksp.0 <- loadDrama("test:rksp.0")
 rksp.0 <- removeSpecialCharacters(rksp.0)
-usethis::use_data(rksp.0, overwrite=TRUE)
+usethis::use_data(rksp.0, overwrite=TRUE, version=2)
 
 rjmw.0 <- loadDrama("test:rjmw.0")
 rjmw.0 <- removeSpecialCharacters(rjmw.0)
-usethis::use_data(rjmw.0, overwrite=TRUE)
+usethis::use_data(rjmw.0, overwrite=TRUE, version=2)
 
 base_dictionary <- loadFields(fieldnames = c("Familie", "Krieg", "Ratio", "Liebe", "Religion"))
 base_dictionary <- lapply(base_dictionary, stringi::stri_trans_general, id="latin-ascii")
-usethis::use_data(base_dictionary, overwrite=TRUE)
+usethis::use_data(base_dictionary, overwrite=TRUE, version=2)
 
